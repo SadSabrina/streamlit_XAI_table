280047a1a1333899418669188ae19679aedc6767
Sabrina Sadiekh <sad.sabrina.d@yandex.ru>
2024-02-27 12:25:20 +0300

    Added new libraries

--- 🇷🇺_RUS.py
+++ 🇷🇺_RUS.py
@@ -37,8 +37,7 @@ st.markdown('''
 
 st.markdown('''Tg: [@sabrina_sadiekh](https://t.me/sabrina_sadiekh)''')
 
-st.markdown('''LinkedIn: [Sabrina Sadiekh](https://www.linkedin.com/in/sabrina-sadiekh-35181a286/?trk=nav_responsive_tab_profile_pic&originalSubdomain=ru)''')
-
+st.markdown('''LinkedIn: [Sabrina Sadiekh](www.linkedin.com/in/sabrina-sadiekh-35181a286)''')
 
 # Data
 merged_table = get_data()
